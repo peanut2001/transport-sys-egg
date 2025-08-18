39a1c8dfb9756319e924c8348eb4336cbd995b1f
向欣柯 <xiangxinke@dolphin-ai.cn>
2025-08-18 13:47:02 +0800

    add: 上传测试文件

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-source /root/.bashrc
-export NODE_ENV=production EGG_SERVER_ENV=prod
-npm install
-npm run build
-rm -rf bin/dist/
-mv -f dist/ bin/
 docker build -t docker.dolphin-ai.cn/library/oa-api ./bin
-docker login --username=Hdolphin+jenkins --password=MSR7VqzImB9pIR779izskan3AXHrr6cH docker.dolphin-ai.cn/library
-docker push docker.dolphin-ai.cn/library/oa-api:latest
+docker login --username=Hlibrary+jenkins --password=LhjbDYsLj0cdk1uXzAojWaALuhyCvFmv docker.dolphin-ai.cn/library
+docker push docker.dolphin-ai.cn/dolphin/dolphin-admin:latest
