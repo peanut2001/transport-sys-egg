256acb94724414168048129beaaf4532fb0e311d
向欣柯 <xiangxinke@dolphin-ai.cn>
2025-08-18 14:07:28 +0800

    add: 上传测试文件

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker build -t docker.dolphin-ai.cn/library/oa-api ./bin
+docker build -t docker.dolphin-ai.cn/library/oa-api .
 docker login --username=Hlibrary+jenkins --password=LhjbDYsLj0cdk1uXzAojWaALuhyCvFmv docker.dolphin-ai.cn/library
 docker push docker.dolphin-ai.cn/dolphin/dolphin-admin:latest
